b4e4500eb2c7ed6894daf7a14aee4d86e91ea60c
Feng <afeng@urban.org>
2019-01-17 13:51:22 -0500

    Highlight correct link in top nav based on the page the user is on

--- js/nav.js
+++ js/nav.js
@@ -8,9 +8,25 @@ d3.select(".peerGroupMenu").on("mouseover", showPeerGroupMenu);
 d3.select(".peerGroupMenu").on("mouseout", hidePeerGroupMenu);
 
 function showPeerGroupMenu() {
+    // open peer group menu
     d3.select(".peerGroupMenu").classed("hidden", false);
+
+    // also make sure "PEER GROUPS" link is selected in top nav and "HOME" is deselected
+    d3.select(".menuLink").classed("selected", true);
+    d3.select(".homeLink").classed("selected", false);
 }
 
 function hidePeerGroupMenu() {
     d3.select(".peerGroupMenu").classed("hidden", true);
+
+    // make sure correct topnav link is selected based on the page the user is on
+    var page = window.location.pathname;
+    if(page === "/") {
+        d3.select(".homeLink").classed("selected", true);
+        d3.select(".menuLink").classed("selected", false);
+    }
+    else {
+        d3.select(".homeLink").classed("selected", false);
+        d3.select(".menuLink").classed("selected", true);
+    }
 }
