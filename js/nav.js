5892d056d4e98d34199bd255b42258d37d2125a0
Feng <afeng@urban.org>
2019-02-05 15:32:39 -0500

    More debugging

--- js/nav.js
+++ js/nav.js
@@ -25,7 +25,7 @@ function hidePeerGroupMenu() {
 
     // make sure correct topnav link is selected based on the page the user is on
     var page = window.location.pathname;
-    console.log(page);
+    console.log(page.indexOf("index.html"));
     if(page.indexOf("index.html" > -1)) {
         d3.select(".homeLink").classed("selected", true);
         d3.select(".menuLink").classed("selected", false);
