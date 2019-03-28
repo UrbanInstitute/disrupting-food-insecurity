c062e7ae384d6652b8bfef3e009510d3117b9e10
Feng <afeng@urban.org>
2019-03-28 16:22:26 -0400

    Make page scroll longer so that the bar transition is less jarring(?) when switching between peer group colors

--- js/charts.js
+++ js/charts.js
@@ -563,7 +563,7 @@ function selectCounty(county) {
 
     // scroll page down to top of dashboard section
     var position = $(".dashboardDrawers").offset().top - $("#header-pinned").height();
-    $("html, body").animate({ scrollTop: position}, 500);
+    $("html, body").animate({ scrollTop: position}, 1000);
 }
 
 function highlightState(stateAbbv, stateName) {
